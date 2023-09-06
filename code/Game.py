986bd2c5a7fd16b2ab45c7c4b515f9daee31dfda
Jefferson Fuckner <sogospel@gmail.com>
2023-09-05 21:35:34 -0400

    Aula prática de Lógica de Programação Aplicada da UNINTER, utlizando Python como linguagem de programação. Nesta aula iremos construir um jogo de nave e tiro em 2D.

--- code/Game.py
+++ code/Game.py
@@ -1,10 +1,20 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
+import pygame
+
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.Menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
-    def run(self, ):
-        pass
-
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            menu = Menu(self.window)
+            menu.run()
+        pygame.quit()
