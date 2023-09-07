0c7a245dc2f4eb842f88672715ed9e91a619ba33
Jefferson Fuckner <sogospel@gmail.com>
2023-09-06 23:55:29 -0400

    Class about creation and movement of entities.

--- code/Level.py
+++ code/Level.py
@@ -1,12 +1,27 @@
-#!/usr/bin/python
-#-*- coding: utf-8 -*-
+import sys
+
+import pygame.display
+from pygame import Surface
+
+from code.Entity import Entity
+from code.EntityFactory import EntityFactory
+
 
 class Level:
-    def __init__(self):
-        self.name = None
-        self.entity_list = None
-        self.window = None
+    def __init__(self, window, name, menu_option):
+        self.name = name
+        self.window: Surface = window
+        self.mode = menu_option
+        self.entity_list: list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
-    def run(self, ):
-        pass
-
+    def run(self):
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
+            pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
