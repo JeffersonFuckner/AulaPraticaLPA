ce5dcde30b35ecaeb3e6dad12586dbe4a05be0ba
Jefferson Fuckner <sogospel@gmail.com>
2023-09-09 16:07:22 -0400

    Class about creation and movement of entities.

--- code/Level.py
+++ code/Level.py
@@ -1,7 +1,10 @@
+import random
 import sys
+from code.Const import C_WHITE, MENU_OPTION, EVENT_ENEMY
 
 import pygame.display
-from pygame import Surface
+from pygame import Surface, Rect
+from pygame.font import Font
 
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
@@ -14,14 +17,33 @@ class Level:
         self.mode = menu_option
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        if menu_option in [MENU_OPTION[1], MENU_OPTION[2]]:
+            self.entity_list.append(EntityFactory.get_entity('Player2'))
+        pygame.time.set_timer(EVENT_ENEMY, 4000)
 
     def run(self):
+        pygame.mixer.music.load(f"./assets/{self.name}.mp3")
+        pygame.mixer.music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(60)
             for ent in self.entity_list:
-                self.window.blit(source=ent.surf, dest=ent.rect)
+                self.window.blit(source=ent.surf, dest=ent.rect)  # Aqui desenho entidades
+                self.level_text(14, f"FPS: {clock.get_fps():.0f}", C_WHITE, (10, 10))
                 ent.move()
             pygame.display.flip()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                if event.type == EVENT_ENEMY:
+                    choice = random.choice(('Enemy1', 'Enemy2'))
+                    self.entity_list.append(EntityFactory.get_entity(choice))
+
+    def level_text(self, text_size: int, text: str, text_color: tuple, text_pos: tuple):
+        text_font: Font = pygame.font.SysFont(name="Menlo", size=text_size)
+        text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
+        text_rect: Rect = text_surf.get_rect(left=text_pos[0], top=text_pos[1])
+        self.window.blit(source=text_surf, dest=text_rect)
